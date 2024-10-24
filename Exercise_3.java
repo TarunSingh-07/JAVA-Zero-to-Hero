72e403dff93dd5990ea28a38dcc882ec4460f769
TarunSingh-07 <tarunsinghtech@gmail.com>
2024-10-25 00:42:23 +0530

    Create Exercise_3.java
    Added exercise 3 conditions

---
+++ Exercise_3.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Exercise_3 {
+    public static void main(String[] args) {
+        /*
+        Create a class Game, which allows a user to play :Guess the Number"
+        game once. Game should have the following methods:
+        1. Constructor to generate the random number
+        2. takeUserInput() to take a user input to number
+        3. isCorrectNumber() to detect whether the number entered by the user is true or false
+        4. getter and setter for noOfGuesses
+        Use properties such as noOfGuesses, etc to get this task done!
+         */
+        
+    }
+}
