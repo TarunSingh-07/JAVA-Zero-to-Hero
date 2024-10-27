0ce5c5e5c20818fa33a162838ea15aba591344e4
TarunSingh-07 <tarunsinghtech@gmail.com>
2024-10-27 14:07:58 +0530

    Update Exercise_3.java
    added exercise 3

--- Exercise_3.java
+++ Exercise_3.java
@@ -2,11 +2,40 @@ package com.company;
 import java.util.Scanner;
 import java.util.Random;
 
-class GuessTheNumber{
-    private int num;
+class Game{
+    public int number;
+    public int inputNumber;
+    public int noOfGuesses;
 
-    public GuessTheNumber(){
+    public int getNoOfGuesses() {
+        return noOfGuesses;
+    }
 
+    public void setNoOfGuesses(int noOfGuesses) {
+        this.noOfGuesses = noOfGuesses;
+    }
+
+// Constructor have no return type .
+    Game(){
+        Random rand = new Random();
+        this.number = rand.nextInt(100);
+    }
+    void takeUserInput(){
+        System.out.println("Guess the number :");
+        Scanner sc = new Scanner(System.in);
+        inputNumber = sc.nextInt();
+    }
+    boolean isCorrectNumber(){
+        if (inputNumber == number){
+            return true;
+        }
+        else if (inputNumber<number){
+            System.out.println("Too less...");
+        }
+        else if (inputNumber>number) {
+            System.out.println("Too high...");
+        }
+        return false;
     }
 }
 
@@ -19,9 +48,16 @@ public class Exercise_3 {
         2. takeUserInput() to take a user input to number
         3. isCorrectNumber() to detect whether the number entered by the user is true or false
         4. getter and setter for noOfGuesses
-        Use properties such as noOfGuesses, etc to get this task done!
+        Use properties such as noOfGuesses, etc. to get this task done!
          */
         System.out.println("Welcome to the Guess the number game");
+        Game g = new Game();
+        boolean b = false;
+        while (!b){
+           g.takeUserInput();
+           b = g.isCorrectNumber();
+           System.out.println(b);
+        }
 
     }
 }
